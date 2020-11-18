0f62a81cd38566aeadcb3b3f8d2fdc414dc506a4
Roy Morla <rjmorla@gmail.com>
2020-11-18 14:19:21 -0800

    global path and removed test statements

--- excellManager.py
+++ excellManager.py
@@ -2,6 +2,8 @@ import numpy as np
 import pandas as pd
 import os
 
+path = os.path.join(os.path.dirname(__file__), 'zoomSheet\zoomSheet.xlsx')
+
 def insertEntry(day, time, meetingID, pw):
     if (pw == ''):
         pw = np.nan
@@ -11,20 +13,15 @@ def insertEntry(day, time, meetingID, pw):
         'MeetingID': [meetingID],
         'Passcode': [pw]
     })
-    path = os.path.join(os.path.dirname(__file__), 'zoomSheet\zoomSheet.xlsx')
     old_df = pd.read_excel(path, 'Sheet1')
     new_df = pd.concat([old_df, df])
     new_df.to_excel(path, 'Sheet1', index=False)
 
 def deleteEntry(day, meetingID):
-    path = os.path.join(os.path.dirname(__file__), 'zoomSheet\zoomSheet.xlsx')
     df = pd.read_excel(path, 'Sheet1')
     df.drop(df[(df.MeetingID == meetingID) & (df.Day == day)].index, inplace=True)
     print(df.Timings)
     df.to_excel(path, 'Sheet1', index=False)
 
-
-path = os.path.join(os.path.dirname(__file__), 'zoomSheet\zoomSheet.xlsx')
-insertEntry('1', '19:45', '11111111111', '')
-deleteEntry(1, 11111111111)
-os.startfile(path)
+def editEntry(day, time, meetingID):
+    print("To Be Implemented")
